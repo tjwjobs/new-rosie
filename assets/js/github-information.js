a3cedd3cfe1ff9b1571493b97484a1de4bc20ca1
tjwjobs <tjwjobs@yahoo.com>
2020-01-31 12:17:57 +0000

    modified

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -64,7 +64,10 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
-            } else {
+            } else if(errprResponse.status === 403){
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too man requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`)
+                } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
